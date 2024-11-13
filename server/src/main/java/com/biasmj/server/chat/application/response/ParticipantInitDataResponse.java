3430b13abc488dd5d99e74b04099e0d87455e763
biasmj016 <mjkim031@gmail.com>
2024-11-13 23:35:35 +0900

    feat: 빠진 response 추가

---
+++ server/src/main/java/com/biasmj/server/chat/application/response/ParticipantInitDataResponse.java
@@ -0,0 +1,30 @@
+package com.biasmj.server.chat.application.response;
+
+import com.biasmj.server.chat.domain.MessageType;
+import com.biasmj.server.chat.domain.type.RequestType;
+import com.biasmj.server.participant.domain.Participant;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ParticipantInitDataResponse extends MessageType {
+    private final List<Participant> participantList;
+
+    public ParticipantInitDataResponse(List<Participant> participantList) {
+        super(RequestType.JOIN);
+        this.participantList = participantList;
+    }
+
+    public List<Participant> getParticipantList() {
+        return participantList;
+    }
+
+    @Override
+    public String toString() {
+        String str = super.getTypeName()+ ":";
+        str += participantList.stream()
+                .map(Participant::id)
+                .collect(Collectors.joining(","));
+        return str;
+    }
+}
