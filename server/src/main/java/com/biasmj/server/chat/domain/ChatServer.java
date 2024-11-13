2c9618e0b620ac0ed53363e92acb761155865ad4
biasmj016 <mjkim031@gmail.com>
2024-11-13 23:35:05 +0900

    feat: ChatServer 추가

---
+++ server/src/main/java/com/biasmj/server/chat/domain/ChatServer.java
@@ -0,0 +1,42 @@
+package com.biasmj.server.chat.domain;
+
+import com.biasmj.server.chat.application.usecase.InitChatData;
+import com.biasmj.server.chat.application.handler.ThreadHandler;
+import com.biasmj.server.chat.application.response.ChatInitDataResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+    private static final Logger logger = LoggerFactory.getLogger(ChatServer.class);
+    public static final List<Socket> sockets = new ArrayList<>();
+
+    public ChatServer() {
+        try (ServerSocket serverSocket = new ServerSocket(9090)) {
+            while (!serverSocket.isClosed()) {
+                logger.info("Waiting for connection...");
+                try {
+                    Socket socket = serverSocket.accept();
+                    logger.info("Participant connected: IP = {}, Port = {}", socket.getInetAddress(), socket.getPort());
+
+                    sockets.add(socket);
+
+                    ThreadHandler thread = new ThreadHandler(socket);
+                    thread.start();
+
+                    InitChatData initChatData = new InitChatData.InitChatDataImpl();
+                    thread.sendMessage(new ChatInitDataResponse(initChatData.finds()));
+                } catch (IOException e) {
+                    logger.error("Error accepting participant connection", e);
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Error starting server", e);
+        }
+    }
+}
